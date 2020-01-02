cb514eaaf22d74ac06817cbd4abf9e5d23219dc8
Rajat Verma <rajatverma5885045@gmail.com>
2020-01-02 17:45:11 +0530

    demo complete

--- react-redux-demo-2/src/App.js
+++ react-redux-demo-2/src/App.js
@@ -10,17 +10,19 @@ import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
 import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake />
+        <UserContainer />
+        {/* <ItemContainer cake />
         <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
       </div>
     </Provider>
   );
